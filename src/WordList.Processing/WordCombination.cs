72d0eed73e27e098313eeddc30c266936dbbc7eb
David Lievrouw <dlievrouw2@csc.com>
2016-05-02 10:15:51 +0200

    Create type WordCombination.

---
+++ src/WordList.Processing/WordCombination.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace WordList.Processing {
+  public class WordCombination {
+    readonly Word _word1;
+    readonly Word _word2;
+
+    public WordCombination(Word word1, Word word2) {
+      if (word1 == null) throw new ArgumentNullException(nameof(word1));
+      if (word2 == null) throw new ArgumentNullException(nameof(word2));
+      _word1 = word1;
+      _word2 = word2;
+    }
+
+    public override string ToString() {
+      return $"{_word1.Value} + {_word2.Value} => {_word1.Value + _word2.Value}";
+    }
+
+    protected bool Equals(WordCombination other) {
+      return _word1.Equals(other._word1) && _word2.Equals(other._word2);
+    }
+
+    public override bool Equals(object obj) {
+      if (ReferenceEquals(null, obj)) return false;
+      if (ReferenceEquals(this, obj)) return true;
+      var other = obj as WordCombination;
+      return other != null && Equals(other);
+    }
+
+    public override int GetHashCode() {
+      unchecked {
+        return (_word1.GetHashCode()*397) ^ _word2.GetHashCode();
+      }
+    }
+
+    public static bool operator ==(WordCombination left, WordCombination right) {
+      return Equals(left, right);
+    }
+
+    public static bool operator !=(WordCombination left, WordCombination right) {
+      return !Equals(left, right);
+    }
+  }
+}
---
+++ src/WordList.Tests/Processing/WordCombinationTests.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using NUnit.Framework;
+using WordList.Processing;
+
+namespace WordList.Tests.Processing {
+  [TestFixture]
+  public class WordCombinationTests {
+    Word _word1;
+    Word _word2;
+    Word _word3;
+
+    [SetUp]
+    public void SetUp() {
+      _word1 = new Word("TheFirstWord");
+      _word2 = new Word("TheSecondWord");
+      _word3 = new Word("TheThirdWord");
+    }
+
+    [TestFixture]
+    public class Construction : WordCombinationTests {
+      [Test]
+      public void GivenNullWords_Throws() {
+        Assert.Throws<ArgumentNullException>(() => new WordCombination(_word1, null));
+        Assert.Throws<ArgumentNullException>(() => new WordCombination(null, _word2));
+      }
+    }
+
+    [TestFixture]
+    public class Equality : WordCombinationTests {
+      [Test]
+      public void IsNotEqualToNull() {
+        var obj1 = new WordCombination(_word1, _word2);
+        WordCombination obj2 = null;
+
+        Assert.That(!Equals(obj1, obj2));
+        Assert.That(obj1 != obj2);
+      }
+
+      [Test]
+      public void NullInstances_AreEqual() {
+        WordCombination obj1 = null;
+        WordCombination obj2 = null;
+
+        Assert.That(Equals(obj1, obj2));
+        Assert.That(obj1 == obj2);
+      }
+
+      [Test]
+      public void ObjectsWithEqualWords_AreEqual() {
+        var obj1 = new WordCombination(_word1, _word2);
+        var obj2 = new WordCombination(_word1, _word2);
+
+        Assert.That(obj1.Equals(obj2));
+        Assert.That(obj1 == obj2);
+        Assert.That(obj1.GetHashCode() == obj2.GetHashCode());
+      }
+
+      [Test]
+      public void ObjectsWithWordsInReversedOrder_AreNotEqual() {
+        var obj1 = new WordCombination(_word1, _word2);
+        var obj2 = new WordCombination(_word2, _word1);
+
+        Assert.That(!obj1.Equals(obj2));
+        Assert.That(obj1 != obj2);
+      }
+
+      [Test]
+      public void ObjectsWithDifferentWords_AreNotEqual() {
+        var obj1 = new WordCombination(_word1, _word2);
+        var obj2 = new WordCombination(_word1, _word3);
+
+        Assert.That(!obj1.Equals(obj2));
+        Assert.That(obj1 != obj2);
+      }
+
+      [Test]
+      public void SubTypeWithEqualWords_AreEqual() {
+        var obj1 = new WordCombination(_word1, _word2);
+        var obj2 = new SubTypeOfWordCombination(_word1, _word2);
+
+        Assert.That(obj1.Equals(obj2));
+        Assert.That(obj1 == obj2);
+        Assert.That(obj1.GetHashCode() == obj2.GetHashCode());
+      }
+
+      class SubTypeOfWordCombination : WordCombination {
+        public SubTypeOfWordCombination(Word word1, Word word2) : base(word1, word2) {}
+      }
+    }
+  }
+}
