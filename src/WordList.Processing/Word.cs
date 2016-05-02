1ba1ae32a87e181a256983f4d0d582bfc4bf8550
David Lievrouw <dlievrouw2@csc.com>
2016-05-02 10:05:45 +0200

    Create the Word class.

---
+++ src/WordList.Processing/Word.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace WordList.Processing {
+  public class Word {
+    public Word(string value) {
+      if (string.IsNullOrWhiteSpace(value)) throw new ArgumentNullException(nameof(value));
+      Value = value;
+    }
+
+    public string Value { get; }
+
+    protected bool Equals(Word other) {
+      return string.Equals(Value, other.Value);
+    }
+
+    public override bool Equals(object obj) {
+      if (ReferenceEquals(null, obj)) return false;
+      if (ReferenceEquals(this, obj)) return true;
+      var other = obj as Word;
+      return other != null && Equals(other);
+    }
+
+    public override int GetHashCode() {
+      return Value.GetHashCode();
+    }
+
+    public static bool operator ==(Word left, Word right) {
+      return Equals(left, right);
+    }
+
+    public static bool operator !=(Word left, Word right) {
+      return !Equals(left, right);
+    }
+  }
+}
---
+++ src/WordList.Tests/Processing/WordTests.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using NUnit.Framework;
+using WordList.Processing;
+
+namespace WordList.Tests.Processing {
+  [TestFixture]
+  public class WordTests {
+    [TestFixture]
+    public class Construction : WordTests {
+      [Test]
+      public void GivenNullValue_Throws() {
+        Assert.Throws<ArgumentNullException>(() => new Word(null));
+      }
+
+      [Test]
+      public void GivenEmptyValue_Throws() {
+        Assert.Throws<ArgumentNullException>(() => new Word(string.Empty));
+      }
+
+      [Test]
+      public void GivenWhiteSpaceValue_Throws() {
+        Assert.Throws<ArgumentNullException>(() => new Word("  " + Environment.NewLine));
+      }
+
+      [Test]
+      public void GivenValidValue_SetsProperty() {
+        var theValue = "The word value!";
+        var actual = new Word(theValue);
+        Assert.That(actual.Value, Is.EqualTo(theValue));
+      }
+    }
+
+    [TestFixture]
+    public class Equality : WordTests {
+      [Test]
+      public void IsNotEqualToNull() {
+        var obj1 = new Word("MyValue");
+        Word obj2 = null;
+
+        Assert.That(!Equals(obj1, obj2));
+        Assert.That(obj1 != obj2);
+      }
+
+      [Test]
+      public void NullInstances_AreEqual() {
+        Word obj1 = null;
+        Word obj2 = null;
+
+        Assert.That(Equals(obj1, obj2));
+        Assert.That(obj1 == obj2);
+      }
+
+      [Test]
+      public void ObjectsWithEqualValues_AreEqual() {
+        var obj1 = new Word("MyValue");
+        var obj2 = new Word("MyValue");
+
+        Assert.That(obj1.Equals(obj2));
+        Assert.That(obj1 == obj2);
+        Assert.That(obj1.GetHashCode() == obj2.GetHashCode());
+      }
+
+      [Test]
+      public void ObjectsWithDifferentCasedValues_AreNotEqual() {
+        var obj1 = new Word("MyValue");
+        var obj2 = new Word("Myvalue");
+
+        Assert.That(!obj1.Equals(obj2));
+        Assert.That(obj1 != obj2);
+      }
+
+      [Test]
+      public void ObjectsWithDifferentValues_AreNotEqual() {
+        var obj1 = new Word("MyValue1");
+        var obj2 = new Word("MyValue2");
+
+        Assert.That(!obj1.Equals(obj2));
+        Assert.That(obj1 != obj2);
+      }
+    }
+  }
+}
