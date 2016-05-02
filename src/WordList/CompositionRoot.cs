b5b385aa096f08fa2eb925d2ad71648bc3b3652a
David Lievrouw <dlievrouw2@csc.com>
2016-05-02 12:59:53 +0200

    Wire up the parts of the solution.

---
+++ src/WordList/CompositionRoot.cs
@@ -0,0 +1,19 @@
+﻿using System.IO;
+using WordList.Data;
+using WordList.Processing;
+
+namespace WordList {
+  public class CompositionRoot {
+    public CompositionRoot() {
+      WordListReader = new WordListReader(
+        new WordListFromFileDataSource(new FileReader(),
+          new FileInfo("wordlist.txt")));
+
+      WordCombinationFinder = new WordCombinationFinder(6);
+    }
+
+    public IWordListReader WordListReader { get; }
+
+    public IWordCombinationFinder WordCombinationFinder { get; }
+  }
+}
--- src/WordList/Program.cs
+++ src/WordList/Program.cs
@@ -1,5 +1,16 @@
-﻿namespace WordList {
-  class Program {
-    static void Main(string[] args) {}
+﻿using System;
+
+namespace WordList {
+  public class Program {
+    static void Main(string[] args) {
+      var compositionRoot = new CompositionRoot();
+
+      var words = compositionRoot.WordListReader.ReadWordList();
+      var combinations = compositionRoot.WordCombinationFinder.FindCombinations(words);
+
+      Console.WriteLine("Press any key to quit...");
+      Console.ReadKey();
+    }
+
   }
 }
