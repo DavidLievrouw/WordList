44479e8c1791bd7d58b7e8be9817174e4d449122
David Lievrouw <dlievrouw2@csc.com>
2016-05-30 11:37:05 +0200

    Add integration test for WordListReader.

--- src/WordList.IntegrationTests/Processing/WordListReaderTests.cs
+++ src/WordList.IntegrationTests/Processing/WordListReaderTests.cs
@@ -1,4 +1,34 @@
-﻿namespace WordList.Processing {
-  class WordListReaderTests {
+﻿using System.IO;
+using System.Linq;
+using NUnit.Framework;
+using WordList.Data;
+
+namespace WordList.Processing {
+  [TestFixture]
+  public class WordListReaderTests {
+    FileInfo _sourceFile;
+    FileReader _fileReader;
+    IWordListDataSource _wordListDataSource;
+    WordListReader _sut;
+
+    [SetUp]
+    public void SetUp() {
+      _fileReader = new FileReader();
+      _sourceFile = new FileInfo(Path.Combine(Path.GetDirectoryName(GetType().Assembly.Location), "wordlist.txt"));
+      _wordListDataSource = new WordListFromFileDataSource(_fileReader, _sourceFile);
+      _sut = new WordListReader(_wordListDataSource);
+    }
+
+    [TestFixture]
+    public class ReadWordList : WordListReaderTests {
+      [Test]
+      public void ReturnsWordForEveryLineInFile() {
+        var expected = File
+          .ReadAllLines(_sourceFile.FullName)
+          .Select(line => new Word(line));
+        var actual = _sut.ReadWordList();
+        Assert.That(actual, Is.EquivalentTo(expected));
+      }
+    }
   }
 }
